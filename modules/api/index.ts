a098f78df24c55c343606a5ab4d424836cabab3e
Noat Eden <noat.eth@gmail.com>
2023-03-10 23:58:35 +0700

    add extend api endpoint

--- modules/api/index.ts
+++ modules/api/index.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import { GlobalProviders } from '../../types/namespaces';
 import { logMiddleware } from './middleware';
 import * as eventRouter from './routes/event';
+import * as extendRouter from './routes/extend';
 import * as parserRouter from './routes/parser';
 
 export function getRouter(providers: GlobalProviders): Router {
@@ -12,6 +13,7 @@ export function getRouter(providers: GlobalProviders): Router {
 
   router.use('/parser', parserRouter.getRouter(providers));
   router.use('/event', eventRouter.getRouter(providers));
+  router.use('/extend', extendRouter.getRouter(providers));
 
   return router;
 }
