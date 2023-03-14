2d1387b96549b17db53157fceea36b4b896c2456
Noat Eden <noat.eth@gmail.com>
2023-03-14 13:15:50 +0700

    🔨 fix: genesis block

--- configs/contracts/balancer.ts
+++ configs/contracts/balancer.ts
@@ -1,6 +1,5 @@
 import { Contract } from '../../types/configs';
 import BalancerVaultAbi from '../abi/balancer/Vault.json';
-import { WorkerGenesisBlocks } from '../index';
 
 export const BalancerContracts: Array<Contract> = [
   {
@@ -8,7 +7,7 @@ export const BalancerContracts: Array<Contract> = [
     protocol: 'balancer',
     abi: BalancerVaultAbi,
     address: '0xba12222222228d8ba445958a75a0704d566bf2c8',
-    birthday: WorkerGenesisBlocks.ethereum,
+    birthday: 16308190,
     events: ['Swap', 'PoolBalanceChanged', 'FlashLoan'],
   },
 ];
