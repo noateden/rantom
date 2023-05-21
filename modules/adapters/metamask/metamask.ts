b05bb3c21636646a9fe40152305c27d9840ceee4
Noat Eden <noat.eth@gmail.com>
2023-05-21 19:33:33 +0700

    fix: metamask contract indexer

--- modules/adapters/metamask/metamask.ts
+++ modules/adapters/metamask/metamask.ts
@@ -46,6 +46,7 @@ export class MetamaskAdapter extends Adapter {
     const signature = topics[0];
     if (this.config.contracts[chain] && this.config.contracts[chain].indexOf(normalizeAddress(address)) !== -1) {
       const web3 = new Web3(EnvConfig.blockchains[chain].nodeRpc);
+      const transaction = await web3.eth.getTransaction(options.hash as string);
       const context = await web3.eth.getTransactionReceipt(options.hash as string);
 
       if (this.config.contracts[chain] && this.config.contracts[chain].indexOf(normalizeAddress(context.to)) !== -1) {
@@ -54,104 +55,106 @@ export class MetamaskAdapter extends Adapter {
         const trader = normalizeAddress(event.trader);
 
         // parse transaction input
-        if (options.input) {
-          const functionSignature = options.input.slice(0, 10);
-          if (functionSignature === Functions.metamaskRouterSwap) {
-            try {
-              const transactionParams = web3.eth.abi.decodeParameters(
-                ['string', 'address', 'uint256', 'bytes'],
-                options.input.slice(10)
-              );
-              const token0 = await this.getWeb3Helper().getErc20Metadata(chain, transactionParams[1].toString());
-              if (token0) {
-                const amount0 = new BigNumber(transactionParams[2].toString())
-                  .dividedBy(new BigNumber(10).pow(token0.decimals))
-                  .toString(10);
+        if (!options.input) {
+          options.input = transaction.input;
+        }
 
-                let token1Address = null;
-                let amount1 = '0';
-                if (transactionParams[0] === 'airswapLight3FeeDynamic') {
-                  const callData = transactionParams[3].slice(0, 322);
-                  const parsedParams = web3.eth.abi.decodeParameters(
-                    ['uint256', 'uint256', 'address', 'address', 'uint256'],
-                    callData
-                  );
-                  token1Address = parsedParams[3].toString();
-                  amount1 = parsedParams[4].toString();
-                } else {
-                  const callData = transactionParams[3].slice(0, 258);
-                  const parsedParams = web3.eth.abi.decodeParameters(
-                    ['address', 'address', 'uint256', 'uint256'],
-                    callData
-                  );
-                  token1Address = parsedParams[1].toString();
-                  amount1 = parsedParams[3].toString();
-                }
+        const functionSignature = options.input.slice(0, 10);
+        if (functionSignature === Functions.metamaskRouterSwap) {
+          try {
+            const transactionParams = web3.eth.abi.decodeParameters(
+              ['string', 'address', 'uint256', 'bytes'],
+              options.input.slice(10)
+            );
+            const token0 = await this.getWeb3Helper().getErc20Metadata(chain, transactionParams[1].toString());
+            if (token0) {
+              const amount0 = new BigNumber(transactionParams[2].toString())
+                .dividedBy(new BigNumber(10).pow(token0.decimals))
+                .toString(10);
 
-                const token1 = await this.getWeb3Helper().getErc20Metadata(chain, token1Address);
-                if (token1) {
-                  amount1 = new BigNumber(amount1).dividedBy(new BigNumber(10).pow(token1.decimals)).toString(10);
+              let token1Address = null;
+              let amount1 = '0';
+              if (transactionParams[0] === 'airswapLight3FeeDynamic') {
+                const callData = transactionParams[3].slice(0, 322);
+                const parsedParams = web3.eth.abi.decodeParameters(
+                  ['uint256', 'uint256', 'address', 'address', 'uint256'],
+                  callData
+                );
+                token1Address = parsedParams[3].toString();
+                amount1 = parsedParams[4].toString();
+              } else {
+                const callData = transactionParams[3].slice(0, 258);
+                const parsedParams = web3.eth.abi.decodeParameters(
+                  ['address', 'address', 'uint256', 'uint256'],
+                  callData
+                );
+                token1Address = parsedParams[1].toString();
+                amount1 = parsedParams[3].toString();
+              }
 
-                  // the amount 1 is the minimum amount was passed by metamask
-                  // we need to find out the exact amount trader was received
-                  // by looking into token1 transfer to trader address
+              const token1 = await this.getWeb3Helper().getErc20Metadata(chain, token1Address);
+              if (token1) {
+                amount1 = new BigNumber(amount1).dividedBy(new BigNumber(10).pow(token1.decimals)).toString(10);
 
-                  for (const log of context.logs) {
-                    if (compareAddress(log.address, token1.address) && log.topics[0] === Signatures.Transfer) {
-                      const transferEvent = web3.eth.abi.decodeLog(
-                        [
-                          {
-                            indexed: true,
-                            internalType: 'address',
-                            name: 'from',
-                            type: 'address',
-                          },
-                          {
-                            indexed: true,
-                            internalType: 'address',
-                            name: 'to',
-                            type: 'address',
-                          },
-                          {
-                            indexed: false,
-                            internalType: 'uint256',
-                            name: 'value',
-                            type: 'uint256',
-                          },
-                        ],
-                        log.data,
-                        log.topics.slice(1)
-                      );
-                      if (compareAddress(transferEvent.to, trader)) {
-                        amount1 = new BigNumber(transferEvent.value.toString())
-                          .dividedBy(new BigNumber(10).pow(token1.decimals))
-                          .toString(10);
-                      }
+                // the amount 1 is the minimum amount was passed by metamask
+                // we need to find out the exact amount trader was received
+                // by looking into token1 transfer to trader address
+
+                for (const log of context.logs) {
+                  if (compareAddress(log.address, token1.address) && log.topics[0] === Signatures.Transfer) {
+                    const transferEvent = web3.eth.abi.decodeLog(
+                      [
+                        {
+                          indexed: true,
+                          internalType: 'address',
+                          name: 'from',
+                          type: 'address',
+                        },
+                        {
+                          indexed: true,
+                          internalType: 'address',
+                          name: 'to',
+                          type: 'address',
+                        },
+                        {
+                          indexed: false,
+                          internalType: 'uint256',
+                          name: 'value',
+                          type: 'uint256',
+                        },
+                      ],
+                      log.data,
+                      log.topics.slice(1)
+                    );
+                    if (compareAddress(transferEvent.to, trader)) {
+                      amount1 = new BigNumber(transferEvent.value.toString())
+                        .dividedBy(new BigNumber(10).pow(token1.decimals))
+                        .toString(10);
                     }
                   }
-
-                  return {
-                    protocol: this.config.protocol,
-                    action: 'trade',
-                    addresses: [trader],
-                    tokens: [token0, token1],
-                    tokenAmounts: [amount0, amount1],
-                    readableString: `${trader} trade ${amount0} ${token0.symbol} for ${amount1} ${token1.symbol} on ${this.config.protocol} chain ${chain}`,
-                  };
                 }
-              }
-            } catch (e: any) {
-              logger.onDebug({
-                service: this.name,
-                message: 'failed to parse transaction params',
-                props: {
-                  chain: chain,
+
+                return {
                   protocol: this.config.protocol,
-                  transaction: options.hash,
-                  error: e.message,
-                },
-              });
+                  action: 'trade',
+                  addresses: [trader],
+                  tokens: [token0, token1],
+                  tokenAmounts: [amount0, amount1],
+                  readableString: `${trader} trade ${amount0} ${token0.symbol} for ${amount1} ${token1.symbol} on ${this.config.protocol} chain ${chain}`,
+                };
+              }
             }
+          } catch (e: any) {
+            logger.onDebug({
+              service: this.name,
+              message: 'failed to parse transaction params',
+              props: {
+                chain: chain,
+                protocol: this.config.protocol,
+                transaction: options.hash,
+                error: e.message,
+              },
+            });
           }
         }
       }
--- tests/actions.ts
+++ tests/actions.ts
@@ -1,8 +1,6 @@
 import { expect } from 'chai';
 import { describe } from 'mocha';
-import Web3 from 'web3';
 
-import EnvConfig from '../configs/envConfig';
 import { normalizeAddress } from '../lib/helper';
 import { ActionTestLogs } from './configs/actionTests';
 
@@ -11,13 +9,6 @@ describe('parse transaction actions', async function () {
     it(`should parse actions correctly ${(item.adapter.config.protocol + ':' + item.action).padEnd(30)} ${item.chain}:${
       item.hash
     }`, async function () {
-      let input = undefined;
-
-      if (item.adapter.name === 'adapter.metamask') {
-        const web3 = new Web3(EnvConfig.blockchains[item.chain].nodeRpc);
-        const transaction = await web3.eth.getTransaction(item.hash);
-        input = transaction.input;
-      }
       const action = await item.adapter.tryParsingActions({
         chain: item.chain,
         sender: normalizeAddress(item.sender),
@@ -25,7 +16,6 @@ describe('parse transaction actions', async function () {
         topics: item.log.topics,
         data: item.log.data,
         hash: item.hash,
-        input,
       });
 
       expect(action).to.not.equal(null);
