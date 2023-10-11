267c51179b7bea0b741e8e2909242e9ad8149dd5
Noat Eden <noat.eth@gmail.com>
2023-10-11 14:08:42 +0700

    add crvusd sfrxETH v2 and tBTC

--- configs/protocols.ts
+++ configs/protocols.ts
@@ -2300,9 +2300,11 @@ export const CrvusdConfigs: ProtocolConfig = {
   contracts: {
     ethereum: [
       '0x8472a9a7632b173c8cf3a86d3afec50c35548e76', // sfrxETH
+      '0xec0820efafc41d8943ee8de495fc9ba8495b15cf', // sfrxETH v2
       '0x100daa78fc509db39ef7d04de0c1abd299f4c6ce', // wstETH
       '0x4e59541306910ad6dc1dac0ac9dfb29bd9f15c67', // WBTC
       '0xa920de414ea4ab66b97da1bfe9e6eca7d4219635', // ETH
+      '0x1c91da0223c763d2e0173243eadaa0a2ea47e704', // tBTC
     ],
   },
 };
--- modules/adapters/crvusd/crvusd.ts
+++ modules/adapters/crvusd/crvusd.ts
@@ -19,9 +19,11 @@ const Signatures = {
 
 const Collaterals: { [key: string]: Token } = {
   '0x8472a9a7632b173c8cf3a86d3afec50c35548e76': Tokens.ethereum.sfrxETH,
+  '0xec0820efafc41d8943ee8de495fc9ba8495b15cf': Tokens.ethereum.sfrxETH,
   '0x100daa78fc509db39ef7d04de0c1abd299f4c6ce': Tokens.ethereum.wstETH,
   '0x4e59541306910ad6dc1dac0ac9dfb29bd9f15c67': Tokens.ethereum.WBTC,
   '0xa920de414ea4ab66b97da1bfe9e6eca7d4219635': Tokens.ethereum.ETH,
+  '0x1c91da0223c763d2e0173243eadaa0a2ea47e704': Tokens.ethereum.tBTC,
 };
 
 export class CrvusdAdapter extends Adapter {
