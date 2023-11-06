3ad0a0d5911b141cebf173b9c928a3a806afba9f
Noat Eden <noat.eth@gmail.com>
2023-11-07 02:31:13 +0700

    hotfix: update sync block number

--- configs/index.ts
+++ configs/index.ts
@@ -2,11 +2,11 @@ export const ParserVersion = '1.0.1';
 
 // return initial block for worker syncing
 export const WorkerGenesisBlocks: { [key: string]: number } = {
-  ethereum: 16308190, // Jan-01-2023 12:00:11 AM +UTC
-  arbitrum: 119354719, // Aug-08-2023 11:15:41 AM +UTC
-  base: 3100110, // Aug-25-2023 06:52:47 PM +UTC
-  optimism: 111059574, // Oct-19-2023 12:18:45 PM +UTC
-  polygon: 48903136, // Oct-19-2023 12:18:56 PM +UTC
+  ethereum: 18415016, // Oct-23-2023 07:20:59 PM +UTC
+  arbitrum: 145790300, // Oct-31-2023 05:15:44 PM +UTC
+  base: 6154823, // Nov-04-2023 11:56:33 AM +UTC
+  optimism: 111150115, // Oct-21-2023 02:36:47 PM +UTC
+  polygon: 49003136, // Oct-22-2023 02:17:48 AM +UTC
 };
 
 export const ApiQueryLogsCachingTime = 10 * 60; // 10 minutes
--- configs/protocols.ts
+++ configs/protocols.ts
@@ -631,7 +631,7 @@ export const CompoundConfigs: ProtocolConfig = {
       '0x6c8c6b02e7b2be14d4fa6022dfd6d75921d90e4e', // cBAT
       '0x70e36f6bf80a52b3b46b3af8e106cc0ed743e8e4', // cCOMP
       '0x5d3a536e4d6dbd6114cc1ead35777bab948e3643', // cDAI
-      '0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5', // cETH
+      '0x4ddc2d193948926d02f9b1fe9e1daa0718270ed5', // cETHz`
       '0x7713dd9ca933848f6819f38b8352d9a15ea73f67', // cFEI
       '0xface851a4921ce59e912d19329929ce6da6eb0c7', // cLINK
       '0x95b4ef2869ebd94beb4eee400a99824bf5dc325b', // cMKR
